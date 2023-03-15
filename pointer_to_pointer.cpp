3c77edc6859efee07dd74e02b8bd4777232b00cd
Minh_Hung <hungm0434@gmail.com>
2023-03-15 07:00:38 +0700

    p2p

---
+++ pointer_to_pointer.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void foo(int **g, int **&h){
+    (**g)++;
+    (*g)++;
+    g++;
+    (**h)++;
+    (*h)++;
+    h++;
+}
+int main(){
+    int a[10];
+    cout<<a[0]<<"\n"<<a[1]<<"\n";
+    int *p = a;
+    int **q = &p;
+    int **r = &p;
+    foo(q,r);
+    cout<<a[0]<<"\n"<<a[1]<<"\n";
+    return 0;
+}
